a7c4a9a23b11b2e9d10ea6312fcb2770861c624c
Clinton Ogada <clintonogada@gmail.com>
2023-03-18 20:42:47 +0300

    Create InequalityOperator.pas
    This program returns true if the two values being compared are not equal

---
+++ InequalityOperator.pas
@@ -0,0 +1,12 @@
+Program InequalityOperator(i,o);
+var
+ num1, num2: integer;
+begin
+ writeln('Enter two numbers:');
+ readln(num1, num2);
+ 
+ if num1 <> num2 then
+ writeln('The numbers are not equal.')
+ else
+ writeln('The numbers are equal.');
+end.
