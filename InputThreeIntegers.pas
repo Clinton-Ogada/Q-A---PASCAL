1cba1f53b0248dd38d8244a846bdeac44fcaa5c2
Clinton Ogada <clintonogada@gmail.com>
2023-03-24 13:26:04 +0300

    Create InputThreeIntegers.pas
    This Pascal program prompts the user to input three integers and stores them in variables num1, num2, and num3:

---
+++ InputThreeIntegers.pas
@@ -0,0 +1,14 @@
+program InputThreeIntegers;
+
+var
+  num1, num2, num3: integer;
+
+begin
+  writeln('Please enter three integers:');
+  readln(num1);
+  readln(num2);
+  readln(num3);
+  
+  writeln('You entered:', num1, ',', num2, ',', num3);
+  
+end.
